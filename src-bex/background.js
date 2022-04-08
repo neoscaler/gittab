20fe975e643e2e28d3dbd066c0b1ffef5cc8dcd1
Julian Meisel <1023650+NeoScaler@users.noreply.github.com>
2022-04-08 22:05:30 +0200

    Update to Manifest v3

--- src-bex/background.js
+++ src-bex/background.js
@@ -1,11 +1,13 @@
 
-chrome.browserAction.onClicked.addListener((/* tab */) => {
-  // Opens our extension in a new browser window.
-  // Only if a popup isn't defined in the manifest.
-  chrome.tabs.create({
-    url: chrome.extension.getURL('www/index.html')
-  }, (/* newTab */) => {
-    // Tab opened.
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.onClicked.addListener((/* tab */) => {
+    // Opens our extension in a new browser window.
+    // Only if a popup isn't defined in the manifest.
+    chrome.tabs.create({
+      url: chrome.runtime.getURL('www/index.html')
+    }, (/* newTab */) => {
+      // Tab opened.
+    })
   })
 })
 
